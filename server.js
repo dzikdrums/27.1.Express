dc5e8a3cd27d0d17f86486e5ed20fccb66549aeb
Maciej Dzik <dzikdrums@gmail.com>
2020-01-23 08:09:10 +0100

    add handlebars

--- server.js
+++ server.js
@@ -1,35 +1,39 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
-
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname + `/views/${name}`))
-  };
-  next();
-})
+app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/', (req, res) => {
-  res.show('index.html');
-})
+  res.render('index');
+});
 
-app.get('/home', (req, res) => {
-  res.show('index.html');
-})
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
+});
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
-})
+  res.render('about', { layout: 'dark' });
+});
 
-app.use('/user', (req, res, next) => {
-  res.send('sorry no access');
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
 });
 
 app.use((req, res) => {
-  res.status(404).show('../public/error404.png');
+  res.status(404).send('404 not found...');
 })
 
 app.listen(8000, () => {
